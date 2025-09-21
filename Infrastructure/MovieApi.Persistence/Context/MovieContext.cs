ae51d765098f9a419d1f18cdb55c12d700f6fe7b
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-09-21 15:28:34 +0300

    add-migration mig1 update-database

--- Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -12,7 +12,7 @@ namespace MovieApi.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost;initial Catalog=ApiMovieDb;integrated Security=true");
+            optionsBuilder.UseSqlServer("Server=localhost;initial Catalog=ApiMovieDb;integrated Security=true;TrustServerCertificate=true;");
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Movie> Movies { get; set; }
