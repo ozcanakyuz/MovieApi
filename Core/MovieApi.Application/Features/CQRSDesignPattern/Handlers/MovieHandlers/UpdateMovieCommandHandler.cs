39db01b47e6e8e962ec83a44cc4a4414550adcb3
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-09-22 11:35:07 +0300

    Handler

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Handlers/MovieHandlers/UpdateMovieCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands;
+using MovieApi.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Handlers.MovieHandlers
+{
+    public class UpdateMovieCommandHandler
+    {
+        private readonly MovieContext _context;
+
+        public UpdateMovieCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+        public async void Handle(UpdateMovieCommand command)
+        {
+            var value = await _context.Movies.FindAsync(command.MovieId);
+            value.CoverImageUrl = command.CoverImageUrl;
+            value.CreatedYear = command.CreatedYear;
+            value.Description = command.Description;
+            value.Duration = command.Duration;
+            value.Rating = command.Rating;
+            value.ReleaseDate = command.ReleaseDate;
+            value.Status = command.Status;
+            value.Title = command.Title;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
