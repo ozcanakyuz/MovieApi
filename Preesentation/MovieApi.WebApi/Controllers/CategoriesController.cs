fb6cc5cc94bfe48dc6f7388bc74fc621e8b3b254
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-09-22 11:43:58 +0300

    Get Handler

---
+++ Preesentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
